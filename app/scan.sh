06d6d31b7adf412e15818c339f22ced1845ff357
Maximilian Köstler <maximilian@koestler.dev>
2021-02-14 16:06:53 +0000

    Add stderr output to log file

--- app/scan.sh
+++ app/scan.sh
@@ -4,7 +4,7 @@ if [ ! -f /tmp/scan_in_progress ]; then
     touch /tmp/scan_in_progress
 
     echo "Start of scan: $(date)" > /var/log/duc.log
-    /usr/local/bin/duc index --progress /scan >> /var/log/duc.log
+    /usr/local/bin/duc index --progress /scan >> /var/log/duc.log 2>&1
     echo "End of scan: $(date)" >> /var/log/duc.log
 
     rm -f /tmp/scan_in_progress
